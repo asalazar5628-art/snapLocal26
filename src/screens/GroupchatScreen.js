6d88d49a36c90a55820c126e9f72f08f767dd000
Jairm245 <jairm245@gmail.com>
2026-07-22 15:42:25 -0700

    Added a response features when you text in the chat you get a response back, hardcoded but it works

--- src/screens/GroupchatScreen.js
+++ src/screens/GroupchatScreen.js
@@ -28,6 +28,50 @@ export default function GroupchatScreen({ route }) {
   user?.user_metadata?.full_name || 
   user?.email?.split('@')[0] || 
   "ME";
+    const [responseIndex,setResponseIndex] = useState(0);
+  const [premadeResponses,setpremadeResponses] = useState(
+    ["I recommend Provecho CaliMex! It's got authentic flavors and it's not that expensive!","YEEESSSS","dude do not go there the food took forever to come out",
+        "There's a little cafe on the way that is usually really quiet","My wife left me and took the kids"
+
+  ]);
+
+  const [responses, setResponses] = useState([
+    {
+      id: "1",
+      sender: "bot1",
+      name: "Bob",
+      text: "",
+      color: "#00A7B5",
+    },
+    {
+      id: "2",
+      sender: "bot2",
+      name: "Sarah",
+      text: "",
+      color: "#CA48D1",
+    },
+    {
+        id: "3",
+        sender: "bot3",
+        name: "David",
+        text: "",
+        color: "#2AF706"
+    },
+    {
+        id: "4",
+        sender: "bot4",
+        name: "Alex",
+        text: "",
+        color: "#AF8B7D"
+    },
+    {
+        id: "5",
+        sender: "bot5",
+        name: "Viola",
+        text: "Lets go there sometime!",
+        color: "#ffcd61"
+    }
+  ]);
     
   const [activeUsername, setActiveUsername] = useState(defaultUsername);
 
@@ -81,20 +125,37 @@ export default function GroupchatScreen({ route }) {
   function sendMessage() {
     if (!message.trim()) return;
 
-    setMessages([
-      ...messages,
-      {
-        id: Date.now().toString(),
-        sender: "me",
-        name: activeUsername,
-        text: message,
-        color: "#FF2D55",
-      },
-    ]);
+    const userMsg = {
+    id: Date.now().toString(),
+    sender: "me",
+    name: activeUsername,
+    text: message,
+    color: "#FF2D55",
+  }; // the format the user message is in
 
-    setMessage("");
+    setMessages((prev) => [...prev, userMsg]); // updates the user message
+        setMessage("");
+        
+    if(responseIndex < premadeResponses.length) // this function is meant to give a reponse based on if I have any premade responses left
+    {
+        const currentBot = responses[responseIndex % responses.length]; // we choose a bot from a premade list
+        const botText = premadeResponses[responseIndex]; // we choose the text from a premade response array
+        setTimeout(() => {
+      const botMsg = {
+        id: (Date.now() + 1).toString(),
+        sender: currentBot.sender,
+        name: currentBot.name,
+        text: botText,
+        color: currentBot.color,
+      };
+
+      setMessages((prev) => [...prev, botMsg])
+      setResponseIndex((prevIndex)=> prevIndex +1);
+      },1000) // this function will make a variable botMsg that will set its id, sender, name, txtmsg, and color based on the attrbutes of the bot list
+                //then set that message, add one to the index to choose the next response msg added timeout to feel more "real"
+    }
   }
-
+  
   function renderMessage({ item }) {
     return (
       <View style={styles.messageWrapper}>
@@ -296,3 +357,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
+
